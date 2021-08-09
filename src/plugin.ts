0bc3461f58699248879f5d644cf82e1b80968cad
Marco Prontera <50167854+Marco-Prontera@users.noreply.github.com>
2021-08-09 23:02:04 +0200

    Basic plugin implementation (#1)
    * Add gitignore
    
    * Add tsconfig
    
    * Add package declarations
    
    * Add plugin code
    
    * Add README
    
    * Try version
    
    * Add DOM to tsconfig
    
    * Use IIFE to add style
    
    * Update version
    
    * Add code inline

---
+++ src/plugin.ts
@@ -0,0 +1,79 @@
+import { IndexHtmlTransformContext, IndexHtmlTransformResult, Plugin } from 'vite';
+
+/**
+ * Inject the CSS compiled with JS.
+ *
+ * @return {Plugin}
+ */
+export default function cssInjectedByJsPlugin(): Plugin {
+
+    return {
+        apply: 'build',
+        enforce: 'post',
+        name: 'css-in-js-plugin',
+        generateBundle(opts, bundle) {
+
+            let styleCode = '';
+
+            for (const key in bundle) {
+
+                if (bundle[key]) {
+
+                    const chunk = bundle[key];
+
+                    if (chunk.type === 'asset' && chunk.fileName.includes('.css')) {
+
+                        styleCode += chunk.source;
+                        delete bundle[key];
+
+                    }
+
+                }
+
+            }
+
+            for (const key in bundle) {
+
+                if (bundle[key]) {
+
+                    const chunk = bundle[key];
+
+                    if (chunk.type === 'chunk' && chunk.fileName.includes('.js')) {
+
+                        //IIFE http://benalman.com/news/2010/11/immediately-invoked-function-expression/
+                        chunk.code += `(function(){ try {var elementStyle = document.createElement('style'); elementStyle.innerText = \`${styleCode}\`; document.head.appendChild(elementStyle);} catch(e) {console.error(e, 'vite-plugin-css-injected-by-js: can't add the style.');} })()`;
+
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        },
+        transformIndexHtml: {
+            enforce: "post",
+            transform(html: string, ctx?: IndexHtmlTransformContext): IndexHtmlTransformResult {
+
+                if (!ctx || !ctx.bundle) return html;
+
+                for (const [, value] of Object.entries(ctx.bundle)) {
+
+                    if (value.fileName.endsWith('.css')) {
+
+                        // Remove CSS link from HTML generated.
+                        const reCSS = new RegExp(`<link rel="stylesheet"[^>]*?href="/${value.fileName}"[^>]*?>`);
+                        html = html.replace(reCSS, '');
+
+                    }
+
+                }
+
+                return html;
+
+            },
+        },
+    };
+
+}
