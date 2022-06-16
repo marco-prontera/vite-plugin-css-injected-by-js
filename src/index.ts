7687b587f02e888dd116ef634c40623b99ffd226
Marco Prontera <prontera.marco.pm@gmail.com>
2022-06-16 18:36:41 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import {
-    IndexHtmlTransformContext,
-    IndexHtmlTransformResult,
-    Plugin,
-} from 'vite';
+import { build, Plugin } from 'vite';
+import { OutputAsset, OutputChunk } from 'rollup';
 
 /**
  * Inject the CSS compiled with JS.
  *
  * @return {Plugin}
  */
-function cssInjectedByJsPlugin(
+export default function cssInjectedByJsPlugin(
     { topExecutionPriority, styleId } = {
         topExecutionPriority: true,
         styleId: '',
@@ -22,87 +19,107 @@ function cssInjectedByJsPlugin(
         apply: 'build',
         enforce: 'post',
         name: 'css-in-js-plugin',
-        generateBundle(opts, bundle) {
-            let styleCode = '';
+        async generateBundle(opts, bundle) {
+            const htmlFiles = Object.keys(bundle).filter((i) => i.endsWith('.html'));
+            const cssAssets = Object.keys(bundle).filter((i) => bundle[i].type == 'asset' && bundle[i].fileName.endsWith('.css'));
+            const jsAssets = Object.keys(bundle).filter(
+                (i) =>
+                    bundle[i].type == 'chunk' &&
+                    bundle[i].fileName.match(/.[cm]?js$/) != null &&
+                    !bundle[i].fileName.includes('polyfill')
+            );
 
-            for (const key in bundle) {
-                if (bundle[key]) {
-                    const chunk = bundle[key];
+            for (const name of htmlFiles) {
+                const htmlChunk = bundle[name] as OutputAsset;
+                let replacedHtml = htmlChunk.source as string;
 
-                    if (
-                        chunk.type === 'asset' &&
-                        chunk.fileName.includes('.css')
-                    ) {
-                        styleCode += chunk.source;
-                        delete bundle[key];
-                    }
+                const allCssCode = cssAssets.reduce(function extractCssCodeAndDeleteFromBundle(previousValue, cssName) {
+                    const cssAsset = bundle[cssName] as OutputAsset;
+                    const result = previousValue + cssAsset.source;
+                    delete bundle[cssName];
+                    replacedHtml = removeLinkStyleSheets(replacedHtml, cssName);
+                    htmlChunk.source = replacedHtml;
+                    return result;
+                }, '');
+
+                if (allCssCode.length > 0) {
+                    cssToInject = allCssCode;
                 }
             }
 
-            if (styleCode.length > 0) {
-                cssToInject = styleCode;
+            const jsAsset = bundle[jsAssets[0]] as OutputChunk;
+
+            let topCode: string = '';
+            let bottomCode: string = '';
+            if (topExecutionPriority) {
+                bottomCode = jsAsset.code;
+            } else {
+                topCode = jsAsset.code;
             }
 
-            for (const key in bundle) {
-                if (bundle[key]) {
-                    const chunk = bundle[key];
-
-                    if (
-                        chunk.type === 'chunk' &&
-                        chunk.fileName.match(/.[cm]?js$/) != null &&
-                        !chunk.fileName.includes('polyfill')
-                    ) {
-                        let topCode: string = '';
-                        let bottomCode: string = '';
-                        if (topExecutionPriority) {
-                            bottomCode = chunk.code;
-                        } else {
-                            topCode = chunk.code;
-                        }
-
-                        chunk.code = topCode;
-                        chunk.code +=
-                            "(function(){ try {var elementStyle = document.createElement('style'); elementStyle.appendChild(document.createTextNode(";
-                        chunk.code += JSON.stringify(cssToInject.trim());
-                        chunk.code += ')); ';
-                        if (typeof styleId == 'string' && styleId.length > 0) {
-                            chunk.code += ` elementStyle.id = "${styleId}"; `;
-                        }
-                        chunk.code +=
-                            "document.head.appendChild(elementStyle);} catch(e) {console.error('vite-plugin-css-injected-by-js', e);} })();";
-                        chunk.code += bottomCode;
-
-                        break;
-                    }
-                }
-            }
-        },
-        transformIndexHtml: {
-            enforce: 'post',
-            transform(
-                html: string,
-                ctx?: IndexHtmlTransformContext
-            ): IndexHtmlTransformResult {
-                if (!ctx || !ctx.bundle) return html;
-
-                for (const [, value] of Object.entries(ctx.bundle)) {
-                    if (value.fileName.endsWith('.css')) {
-                        // Remove CSS link from HTML generated.
-                        const reCSS = new RegExp(
-                            `<link rel="stylesheet"[^>]*?href=".*/${value.fileName}"[^>]*?>`
-                        );
-                        html = html.replace(reCSS, '');
-                    }
-                }
-
-                return html;
-            },
+            jsAsset.code = topCode;
+            jsAsset.code += await buildCSSInjectionCode(cssToInject, styleId);
+            jsAsset.code += bottomCode;
         },
     };
 }
 
-module.exports = cssInjectedByJsPlugin;
+function removeLinkStyleSheets(html: string, cssFileName: string): string {
+    const removeCSS = new RegExp(`<link rel="stylesheet"[^>]*?href=".*/${cssFileName}"[^>]*?>`);
+    return html.replaceAll(removeCSS, '');
+}
 
-cssInjectedByJsPlugin.default = cssInjectedByJsPlugin;
+const cssInjectedByJsId = '\0vite/all-css';
 
-export default cssInjectedByJsPlugin;
+async function buildCSSInjectionCode(cssToInject: string, styleId: string): Promise<OutputChunk | null> {
+    const res = await build({
+        root: __dirname,
+        configFile: false,
+        logLevel: 'error',
+        plugins: [singleCSSChunkPlugin(cssToInject, styleId)],
+        build: {
+            write: false,
+            target: 'es2015',
+            minify: 'terser',
+            assetsDir: '',
+            rollupOptions: {
+                input: {
+                    ['all-css']: cssInjectedByJsId,
+                },
+                output: {
+                    format: 'iife',
+                    manualChunks: undefined,
+                },
+            },
+        },
+    });
+    const _cssChunk = Array.isArray(res) ? res[0] : res;
+    if (!('output' in _cssChunk)) return null;
+
+    return _cssChunk.output[0];
+}
+
+/**
+ * @param {string} cssToInject
+ * @param {string|null} styleId
+ * @return {Plugin}
+ */
+function singleCSSChunkPlugin(cssToInject: string, styleId: string | null): Plugin {
+    return {
+        name: 'vite:single-css-chunk-plugin',
+        resolveId(id: string) {
+            if (id === cssInjectedByJsId) {
+                return id;
+            }
+        },
+        load(id: string) {
+            if (id === cssInjectedByJsId) {
+                const cssCode = JSON.stringify(cssToInject.trim());
+
+                return `try{var elementStyle = document.createElement('style');${
+                    typeof styleId == 'string' && styleId.length > 0 ? 'elementStyle.id = "${styleId}";' : ''
+                }elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`;
+            }
+        },
+    };
+}
