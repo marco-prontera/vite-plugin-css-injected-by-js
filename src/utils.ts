c59d0acfe910bfb7c47f2b966e61cba18f50159f
Nick Hudkins <nick@nickhudkins.com>
2023-01-10 00:03:20 -0500

    🔐 Allow `useStrictCSP` to add `nonce` at runtime
    Utilizes `meta[property=csp-nonce]` to fetch `content` for for the `nonce` to be added

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ import { OutputChunk } from 'rollup';
 
 interface InjectCodeOptions {
     styleId?: string;
+    useStrictCSP?: boolean;
 }
 
 export type InjectCode = (cssCode: string, options: InjectCodeOptions) => string;
@@ -10,9 +11,11 @@ export type InjectCodeFunction = (cssCode: string, options: InjectCodeOptions) =
 
 const cssInjectedByJsId = '\0vite/all-css';
 
-const defaultInjectCode: InjectCode = (cssCode, { styleId }) =>
+const defaultInjectCode: InjectCode = (cssCode, { styleId, useStrictCSP }) =>
     `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');${
         typeof styleId == 'string' && styleId.length > 0 ? `elementStyle.id = '${styleId}';` : ''
+    }${
+        useStrictCSP ? `elementStyle.nonce = document.head.querySelector('meta[property=csp-nonce]')?.content;` : ''
     }elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`;
 
 interface BuildCSSInjectionCodeInput {
@@ -20,18 +23,20 @@ interface BuildCSSInjectionCodeInput {
     styleId?: string;
     injectCode?: InjectCode;
     injectCodeFunction?: InjectCodeFunction;
+    useStrictCSP?: boolean;
 }
 export async function buildCSSInjectionCode({
     cssToInject,
     styleId,
     injectCode,
     injectCodeFunction,
+    useStrictCSP,
 }: BuildCSSInjectionCodeInput): Promise<OutputChunk | null> {
     const res = await build({
         root: '',
         configFile: false,
         logLevel: 'error',
-        plugins: [injectionCSSCodePlugin({ cssToInject, styleId, injectCode, injectCodeFunction })],
+        plugins: [injectionCSSCodePlugin({ cssToInject, styleId, injectCode, injectCodeFunction, useStrictCSP })],
         build: {
             write: false,
             target: 'es2015',
@@ -59,6 +64,7 @@ interface InjectionCSSCodePluginInput {
     styleId?: string;
     injectCode?: InjectCode;
     injectCodeFunction?: InjectCodeFunction;
+    useStrictCSP?: boolean;
 }
 
 /**
@@ -72,6 +78,7 @@ function injectionCSSCodePlugin({
     injectCode,
     injectCodeFunction,
     styleId,
+    useStrictCSP,
 }: InjectionCSSCodePluginInput): Plugin {
     return {
         name: 'vite:injection-css-code-plugin',
@@ -84,10 +91,10 @@ function injectionCSSCodePlugin({
             if (id == cssInjectedByJsId) {
                 const cssCode = JSON.stringify(cssToInject.trim());
                 if (injectCodeFunction) {
-                    return `(${injectCodeFunction})(${cssCode}, ${JSON.stringify({ styleId })})`;
+                    return `(${injectCodeFunction})(${cssCode}, ${JSON.stringify({ styleId, useStrictCSP })})`;
                 }
                 const injectFunction = injectCode || defaultInjectCode;
-                return injectFunction(cssCode, { styleId });
+                return injectFunction(cssCode, { styleId, useStrictCSP });
             }
         },
     };
