1d58589d82723e2aa3740a240471cf83d3ace019
Corrado Petrelli <corrado.petrelli@gmail.com>
2023-01-17 18:50:42 +0100

    chore: introduce basic vitest coverage with istanbul

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'happy-dom',
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'json', 'html'],
+        },
     },
 });
