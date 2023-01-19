6256fd8f875c66c98571a9f479172ca33f3c105b
Corrado Petrelli <corrado.petrelli@gmail.com>
2023-01-19 22:52:20 +0100

    [#72] Introduce Istanbul for code coverage reporting (#73)
    * deps: introduce istanbul coverage for vitest
    
    * chore: introduce basic vitest coverage with istanbul
    
    * chore: ignore coverage folder
    
    * chore: ignore console function to coverage
    
    * test: removeLinkStyleSheets
    
    * test: introduce test without styleId for coverage
    
    * test: change default threshold on istanbul
    
    * ci: introduce coverage command on ci

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, vi, beforeAll } from 'vitest';
-import { buildCSSInjectionCode } from '../src/utils';
+import { buildCSSInjectionCode, removeLinkStyleSheets } from '../src/utils';
 
 const onerror = vi.fn();
 window.onerror = onerror;
@@ -27,6 +27,25 @@ test('Generate JS that applies styles', async () => {
 
     // StyleId applied
     expect(document.head.querySelector(`style#${styleId}`)).not.toBeNull();
+
+    // Applied style!
+    expect(getComputedStyle(document.body).color).toBe('red');
+});
+
+test('Generate JS that applies styles, without styleId', async () => {
+    const output = await buildCSSInjectionCode({
+        cssToInject: 'body { color: red; }',
+    });
+
+    const $script = document.createElement('script');
+    $script.textContent = output?.code || 'throw new Error("UNCAUGHT ERROR")';
+    document.head.appendChild($script);
+
+    // Doesn't error
+    expect(onerror).not.toBeCalled();
+
+    // StyleId applied
+    expect(document.head.querySelector(`style`)).not.toBeNull();
 
     // Applied style!
     expect(getComputedStyle(document.body).color).toBe('red');
@@ -117,4 +136,38 @@ test('Generate JS that applies styles from custom code, with a nonce', async () 
 
     // Did we dynamically set the nonce?
     expect(elem?.nonce).toBe('abc-123');
+});
+
+test('Remove link stylesheets', async () => {
+    const htmlGenerate = (cssFileName) => `<link rel="stylesheet" href="${cssFileName}">`;
+    const cssFileName1 = `foo.css`;
+    const cssFileNameDifferent = `bar.css`;
+
+    const tagLinkCssFileName1 = htmlGenerate(cssFileName1);
+    const emptyString = removeLinkStyleSheets(tagLinkCssFileName1, cssFileName1);
+    expect(emptyString).toEqual('');
+
+    const tagLinkNotChanged = removeLinkStyleSheets(tagLinkCssFileName1, cssFileNameDifferent);
+    expect(tagLinkNotChanged).toEqual(tagLinkCssFileName1);
+});
+
+test('Generate JS that applies styles', async () => {
+    const styleId = `style-${Date.now()}`;
+    const output = await buildCSSInjectionCode({
+        cssToInject: 'body { color: red; }',
+        styleId,
+    });
+
+    const $script = document.createElement('script');
+    $script.textContent = output?.code || 'throw new Error("UNCAUGHT ERROR")';
+    document.head.appendChild($script);
+
+    // Doesn't error
+    expect(onerror).not.toBeCalled();
+
+    // StyleId applied
+    expect(document.head.querySelector(`style#${styleId}`)).not.toBeNull();
+
+    // Applied style!
+    expect(getComputedStyle(document.body).color).toBe('red');
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'happy-dom',
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'json', 'html'],
+            skipFull: false,
+            branches: 70,
+            lines: 70,
+            functions: 70,
+            statements: 70
+        },
     },
 });
