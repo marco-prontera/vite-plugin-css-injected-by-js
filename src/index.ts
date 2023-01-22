e4bb8e2497dd6db3acfe4d1eb079a784af47c9aa
Mohammad Seyfayi <mohammad.seyfayi@gmail.com>
2023-01-22 13:06:58 +0330

    fix: add imported css to its importer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { buildCSSInjectionCode, removeLinkStyleSheets, warnLog, debugLog } from './utils.js';
+import { buildCSSInjectionCode, debugLog, removeLinkStyleSheets, warnLog } from './utils.js';
 import { OutputAsset, OutputChunk } from 'rollup';
 import { Plugin, ResolvedConfig } from 'vite';
 import { PluginConfiguration } from './interface';
@@ -18,7 +18,6 @@ export default function cssInjectedByJsPlugin({
     useStrictCSP,
 }: PluginConfiguration | undefined = {}): Plugin {
     //Globally so we can add it to legacy and non-legacy bundle.
-    let cssToInject: string = '';
     let config: ResolvedConfig;
 
     const topExecutionPriorityFlag = typeof topExecutionPriority == 'boolean' ? topExecutionPriority : true;
@@ -41,22 +40,6 @@ export default function cssInjectedByJsPlugin({
             const cssAssets = Object.keys(bundle).filter(
                 (i) => bundle[i].type == 'asset' && bundle[i].fileName.endsWith('.css')
             );
-
-            const allCssCode = cssAssets.reduce(function extractCssCodeAndDeleteFromBundle(previousValue, cssName) {
-                const cssAsset = bundle[cssName] as OutputAsset;
-                const cssAssetSource =
-                    typeof cssAsset.source == 'string'
-                        ? cssAsset.source.replace(/(\r\n|\n|\r)+$/gm, '')
-                        : cssAsset.source;
-                const result = previousValue + cssAssetSource;
-                delete bundle[cssName];
-
-                return result;
-            }, '');
-
-            if (allCssCode.length > 0) {
-                cssToInject = allCssCode;
-            }
 
             for (const name of htmlFiles) {
                 const htmlChunk = bundle[name] as OutputAsset;
@@ -103,29 +86,43 @@ export default function cssInjectedByJsPlugin({
                 });
             }
 
-            const cssInjectionCode = await buildCSSInjectionCode({
-                cssToInject: typeof preRenderCSSCode == 'function' ? preRenderCSSCode(cssToInject) : cssToInject,
-                styleId,
-                injectCode,
-                injectCodeFunction,
-                useStrictCSP,
-            });
-
             if (jsAssetTargets.length == 0) {
                 throw new Error(
                     'Unable to locate the JavaScript asset for adding the CSS injection code. It is recommended to review your configurations.'
                 );
             }
 
-            jsAssetTargets.forEach((jsAsset) => {
+            for (const jsAsset of jsAssetTargets) {
+                // @ts-ignore
+                const cssNamesSet: Set<string> = jsAsset.viteMetadata.importedCss;
+                let cssToInject: string = '';
+                cssNamesSet.forEach((cssName) => {
+                    cssToInject += String(extractCssCode(bundle[cssName] as OutputAsset));
+                });
+
+                // delete bundle[cssName];
+                const cssInjectionCode = await buildCSSInjectionCode({
+                    cssToInject: typeof preRenderCSSCode == 'function' ? preRenderCSSCode(cssToInject) : cssToInject,
+                    styleId,
+                    injectCode,
+                    injectCodeFunction,
+                    useStrictCSP,
+                });
+
                 const appCode = jsAsset.code;
                 jsAsset.code = topExecutionPriorityFlag ? '' : appCode;
                 jsAsset.code += cssInjectionCode ? cssInjectionCode.code : '';
                 jsAsset.code += !topExecutionPriorityFlag ? '' : appCode;
+            }
+            cssAssets.forEach((css) => {
+                delete bundle[css];
             });
         },
     };
 }
+
+const extractCssCode = (cssAsset: OutputAsset) =>
+    typeof cssAsset.source == 'string' ? cssAsset.source.replace(/(\r\n|\n|\r)+$/gm, '') : cssAsset.source;
 
 function isJsOutputChunk(chunk: OutputAsset | OutputChunk): boolean {
     return chunk.type == 'chunk' && chunk.fileName.match(/.[cm]?js$/) != null;
