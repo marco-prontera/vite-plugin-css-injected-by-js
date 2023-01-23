3a2d006113a4644725c8347b1d807a08d6619d87
Mohammad Seyfayi <39955233+mseyfayi@users.noreply.github.com>
2023-01-23 21:05:22 +0330

    [#74] feat: add imported css to its importer (#75)
    * fix: add imported css to its importer
    
    * fix: convert extractCssCode from arrow to named function
    
    * feat: add `relativeCSSInjection` to switch between adding allCSS or imported CSS
    
    Co-authored-by: Mohammad Seyfayi <mohammad.seyfayi@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { buildCSSInjectionCode, removeLinkStyleSheets, warnLog, debugLog } from './utils.js';
+import { buildCSSInjectionCode, debugLog, removeLinkStyleSheets, warnLog } from './utils.js';
 import { OutputAsset, OutputChunk } from 'rollup';
 import { Plugin, ResolvedConfig } from 'vite';
 import { PluginConfiguration } from './interface';
@@ -16,9 +16,9 @@ export default function cssInjectedByJsPlugin({
     styleId,
     topExecutionPriority,
     useStrictCSP,
+    relativeCSSInjection,
 }: PluginConfiguration | undefined = {}): Plugin {
     //Globally so we can add it to legacy and non-legacy bundle.
-    let cssToInject: string = '';
     let config: ResolvedConfig;
 
     const topExecutionPriorityFlag = typeof topExecutionPriority == 'boolean' ? topExecutionPriority : true;
@@ -41,22 +41,6 @@ export default function cssInjectedByJsPlugin({
             const cssAssets = Object.keys(bundle).filter(
                 (i) => bundle[i].type == 'asset' && bundle[i].fileName.endsWith('.css')
             );
-
-            const allCssCode = cssAssets.reduce(function extractCssCodeAndDeleteFromBundle(previousValue, cssName) {
-                const cssAsset = bundle[cssName] as OutputAsset;
-                const cssAssetSource =
-                    typeof cssAsset.source == 'string'
-                        ? cssAsset.source.replace(/(\r\n|\n|\r)+$/gm, '')
-                        : cssAsset.source;
-                const result = previousValue + cssAssetSource;
-                delete bundle[cssName];
-
-                return result;
-            }, '');
-
-            if (allCssCode.length > 0) {
-                cssToInject = allCssCode;
-            }
 
             for (const name of htmlFiles) {
                 const htmlChunk = bundle[name] as OutputAsset;
@@ -103,28 +87,56 @@ export default function cssInjectedByJsPlugin({
                 });
             }
 
-            const cssInjectionCode = await buildCSSInjectionCode({
-                cssToInject: typeof preRenderCSSCode == 'function' ? preRenderCSSCode(cssToInject) : cssToInject,
-                styleId,
-                injectCode,
-                injectCodeFunction,
-                useStrictCSP,
-            });
-
             if (jsAssetTargets.length == 0) {
                 throw new Error(
                     'Unable to locate the JavaScript asset for adding the CSS injection code. It is recommended to review your configurations.'
                 );
             }
 
-            jsAssetTargets.forEach((jsAsset) => {
+            const allCssCode = cssAssets
+                .map((cssName) => bundle[cssName] as OutputAsset)
+                .map(
+                    (cssAsset) =>
+                        (typeof cssAsset.source == 'string'
+                            ? cssAsset.source.replace(/(\r\n|\n|\r)+$/gm, '')
+                            : cssAsset.source) as string
+                )
+                .reduce((previousValue, cssAssetSource) => previousValue + cssAssetSource, '');
+
+            for (const jsAsset of jsAssetTargets) {
+                let cssToInject: string = '';
+
+                if (!relativeCSSInjection && allCssCode.length > 0) {
+                    cssToInject = allCssCode;
+                } else {
+                    // @ts-ignore
+                    const cssNamesSet: Set<string> = jsAsset.viteMetadata.importedCss;
+                    cssNamesSet.forEach((cssName) => {
+                        cssToInject += String(extractCssCode(bundle[cssName] as OutputAsset));
+                    });
+                }
+                const cssInjectionCode = await buildCSSInjectionCode({
+                    cssToInject: typeof preRenderCSSCode == 'function' ? preRenderCSSCode(cssToInject) : cssToInject,
+                    styleId,
+                    injectCode,
+                    injectCodeFunction,
+                    useStrictCSP,
+                });
+
                 const appCode = jsAsset.code;
                 jsAsset.code = topExecutionPriorityFlag ? '' : appCode;
                 jsAsset.code += cssInjectionCode ? cssInjectionCode.code : '';
                 jsAsset.code += !topExecutionPriorityFlag ? '' : appCode;
+            }
+            cssAssets.forEach((css) => {
+                delete bundle[css];
             });
         },
     };
+}
+
+function extractCssCode(cssAsset: OutputAsset) {
+    return typeof cssAsset.source == 'string' ? cssAsset.source.replace(/(\r\n|\n|\r)+$/gm, '') : cssAsset.source;
 }
 
 function isJsOutputChunk(chunk: OutputAsset | OutputChunk): boolean {
