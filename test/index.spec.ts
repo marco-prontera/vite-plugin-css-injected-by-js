2e1509d9d1f8b2897e167fb4a10ea736496843e3
Alex Miller <alex.miller@phocassoftware.com>
2023-02-07 16:13:13 +1300

    test: buildJsCssMap

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,8 @@
-import type { OutputAsset, OutputBundle } from 'rollup';
+import type { OutputAsset, OutputBundle, OutputChunk } from 'rollup';
 import { beforeEach, describe, expect, it } from 'vitest';
 
-import { concatCss, extractCssAndDeleteFromBundle } from '../src/index';
+import { buildJsCssMap, concatCss, extractCssAndDeleteFromBundle } from '../src/index';
+import type { PluginConfiguration } from '../src/interface';
 
 describe('css-injected-by-js', () => {
     let bundle: OutputBundle;
@@ -10,19 +11,19 @@ describe('css-injected-by-js', () => {
         bundle = {
             'a.css': {
                 fileName: 'a.css',
-                name: 'a.css',
+                name: 'a',
                 source: 'a',
                 type: 'asset',
             },
             'b.css': {
                 fileName: 'b.css',
-                name: 'b.css',
+                name: 'b',
                 source: 'b',
                 type: 'asset',
             },
             'c.css': {
                 fileName: 'c.css',
-                name: 'c.css',
+                name: 'c',
                 source: 'c',
                 type: 'asset',
             },
@@ -75,6 +76,81 @@ describe('css-injected-by-js', () => {
             expect(css).toEqual('ac');
             // Only assert numbers here, removal validity tested in `extractCssAndDeleteFromBundle` tests.
             expect(Object.keys(bundle)).toHaveLength(initialBundleSize - toConcat.length);
+        });
+    });
+
+    describe('buildJsCssMap', () => {
+        function generateJsChunk(name: string, importedCss: string[]): OutputChunk {
+            return {
+                code: name,
+                dynamicImports: [],
+                exports: [],
+                facadeModuleId: null,
+                fileName: `${name}.js`,
+                implicitlyLoadedBefore: [],
+                importedBindings: {},
+                imports: [],
+                isDynamicEntry: false,
+                isEntry: true,
+                isImplicitEntry: false,
+                map: null,
+                moduleIds: [],
+                modules: {},
+                name: name,
+                referencedFiles: [],
+                type: 'chunk',
+                viteMetadata: { importedAssets: new Set(), importedCss: new Set(importedCss) },
+            };
+        }
+
+        it('should build a map with a JS assets', () => {
+            bundle['a.js'] = generateJsChunk('a', ['a.css']);
+
+            const assetsMap = buildJsCssMap(bundle);
+
+            expect(Object.keys(assetsMap)).toHaveLength(1);
+            expect(assetsMap['a.js']).toHaveLength(1);
+            expect(assetsMap['a.js']).toContain('a.css');
+        });
+
+        it('should build a map of multiple JS assets', () => {
+            bundle['a.js'] = generateJsChunk('a', ['a.css']);
+            bundle['c.js'] = generateJsChunk('c', ['c.css']);
+
+            const assetsMap = buildJsCssMap(bundle);
+
+            expect(Object.keys(assetsMap)).toHaveLength(2);
+            expect(assetsMap['a.js']).toHaveLength(1);
+            expect(assetsMap['a.js']).toContain('a.css');
+            expect(assetsMap['b.js']).toBeUndefined();
+            expect(assetsMap['c.js']).toHaveLength(1);
+            expect(assetsMap['c.js']).toContain('c.css');
+        });
+
+        it('should build a map with a customer filter', () => {
+            const filter: PluginConfiguration['jsAssetsFilterFunction'] = (chunk: OutputChunk) =>
+                chunk.fileName === 'c.js';
+
+            bundle['a.js'] = generateJsChunk('a', ['a.css']);
+            bundle['b.js'] = generateJsChunk('b', ['b.css']);
+            bundle['c.js'] = generateJsChunk('c', ['c.css']);
+
+            const assetsMap = buildJsCssMap(bundle, filter);
+
+            expect(Object.keys(assetsMap)).toHaveLength(1);
+            expect(assetsMap['a.js']).toBeUndefined();
+            expect(assetsMap['b.js']).toBeUndefined();
+            expect(assetsMap['c.js']).toHaveLength(1);
+            expect(assetsMap['c.js']).toContain('c.css');
+        });
+
+        it('should throw when a filter fails to find a key', () => {
+            const filter: PluginConfiguration['jsAssetsFilterFunction'] = (chunk: OutputChunk) =>
+                chunk.name === 'cake-town';
+
+            expect(() => buildJsCssMap(bundle, filter)).toThrowError(
+                'Unable to locate the JavaScript asset for adding the CSS injection code. It is recommended to review your configurations.'
+            );
         });
     });
 });
